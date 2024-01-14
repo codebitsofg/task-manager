6bcd571ea6288c544497fe1bccd764249a0b8b3f
Berat Genç <beratgenc@Berats-MacBook-Pro.local>
2024-01-14 17:42:32 +0300

    text changed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'This text should be changed on push!';
   }
 }
