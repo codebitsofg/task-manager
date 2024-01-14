336e1039ce3b6a7c0ea405744cbb354cb6b19c2a
Berat Genç <beratgenc@Berats-MacBook-Pro.local>
2024-01-14 14:30:58 +0300

    updated pipelines

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(`--- app is listening on ${process.env.PORT}`)
+  await app.listen(process.env.PORT);
 }
 bootstrap();
