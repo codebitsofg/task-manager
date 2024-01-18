18602d81ec051729c0a05511af2dfc98e06c80ee
Berat Genç <beratgenc@Berats-MacBook-Pro.local>
2024-01-18 13:52:05 +0300

    removed /

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: 'https://app.taskermanager.online/'
+    origin: 'https://app.taskermanager.online'
   })
   console.log(`--- app is listening oooon ${process.env.PORT}`)
   await app.listen(process.env.PORT ?? 3001)
