e7ad19bd5f67f2942e61e40e8db9bb92f67667a6
Berat Genç <beratgenc@Berats-MacBook-Pro.local>
2024-01-18 13:25:09 +0300

    paths added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({ origin: 'http://localhost:3002' })
+  app.enableCors({
+    origin: [
+      'https://task-manager-frontend-7egxgsovaq-ew.a.run.app/',
+      'app.taskermanager.online'
+    ]
+  })
   console.log(`--- app is listening oooon ${process.env.PORT}`)
   await app.listen(process.env.PORT ?? 3001)
 }
